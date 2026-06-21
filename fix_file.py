ee7e8c8cc970cb14cc9a9a3cfa247b983dbbfca1
Philip Z <hello@teaql.io>
2026-06-21 08:25:56 +0800

    chore: remove all python scripts

--- fix_file.py
+++
@@ -1,8 +0,0 @@
-with open("teaql-tool-context/src/std_tools/file.rs", "r") as f:
-    content = f.read()
-
-content = content.replace(" {>", "> {")
-content = content.replace(" >{", "> {")
-
-with open("teaql-tool-context/src/std_tools/file.rs", "w") as f:
-    f.write(content)
--- fix_macros.py
+++
@@ -1,7 +0,0 @@
-with open("teaql-tool-context/src/macros.rs", "r") as f:
-    content = f.read()
-
-content = content.replace("teaql_tool_core::$ret", "teaql_tool_core::MustPurpose<$ret>")
-
-with open("teaql-tool-context/src/macros.rs", "w") as f:
-    f.write(content)
